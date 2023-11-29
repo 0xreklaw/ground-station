02a3d500c4d1b18e1feec0b13473163c5d1d7e4b
Rixhee <rishitbansals@gmail.com>
2023-11-29 18:33:30 -0500

    Updated the arduino code for the esp32 to act as an access point

--- wifiConnectivity/wifiConnectivity.ino
+++ wifiConnectivity/wifiConnectivity.ino
@@ -1,36 +1,47 @@
+/* Usage
+ * run http://192.168.4.1 on the local web browser
+*/
+
 /* INCLUDE LIBRARIES */
 #include <WiFi.h>
+
 /* GLOBALS */
 /* WiFi */
-const char* ssid = "OLIN-DEVICES";
-const char* wpa = "Engineering4Every1!";
-IPAddress ip(192, 168, 0, 100);
-IPAddress gateway(192, 168, 0, 1);
-IPAddress subnet(255, 255, 255, 0);
+const char* ssid = "MiniAvionics";
+const char* wpa = "Aeiou12345?";
+
+WiFiServer server(80);
+
 /* SETUP */
 void setup(){
   Serial.begin(115200);
-  Serial.println();
-  connectWiFi();
+
+  // Set up the access point
+  WiFi.softAP(ssid, wpa);
+
+  // Start the server
+  server.begin();
+  
+  Serial.println("SoftAP started with IP: " + WiFi.softAPIP().toString());
 }
+w
 /* LOOP */
-void loop(){}
-/* METHODS */
-void connectWiFi(){
-  /* Simplified Explanation:
-     STA mode connects to something (router etc),
-     AP mode lets things connect to it */
-  WiFi.enableSTA(true);
-  /* set your devices ip information */
-  WiFi.config(ip, gateway, subnet);
-  /* start the connection */
-  WiFi.begin(ssid, wpa);
-  Serial.print("Connecting to WiFi.");
-  /* Wait for the connection to be finalized */
-  while((!(WiFi.status() == WL_CONNECTED))) {
-    Serial.print(".");
-    delay(300);
+void loop(){
+  // Check if a client has connected
+  WiFiClient client = server.available();
+  if (client) {
+    Serial.println("New client connected");
+
+    // Read the request
+    String request = client.readStringUntil('\r');
+    Serial.println("Request: " + request);
+
+    // Send a response to the client
+    client.println("HTTP/1.1 200 OK");
+    client.println("Content-Type: text/html");
+    client.println("");
+    client.println("<h1>Hello from ESP32!</h1>");
+    client.stop();
+    Serial.println("Client disconnected");
   }
-  Serial.println();
-  Serial.println("Successfully connected to WiFi!");
 }
